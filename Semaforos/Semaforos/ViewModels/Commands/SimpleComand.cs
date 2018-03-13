59401c7baf5cc10f53b741e9f35370778fe3bccf
Fabián Barón <fabian-enrique@hotmail.com>
2018-03-13 18:18:04 -0500

    Iteración Inicial para master

---
+++ Semaforos/Semaforos/ViewModels/Commands/SimpleComand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Semaforos.ViewModels.Commands
+{
+    public class SimpleComand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        public ViewModelBase ViewModelBase { get; set; }
+
+        public SimpleComand(ViewModelBase viewModel)
+        {
+            this.ViewModelBase = viewModel;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            ViewModelBase.SimpreMethod();
+        }
+    }
+}
---
+++ Semaforos/Semaforos/ViewModels/ViewModelBase.cs
@@ -0,0 +1,25 @@
+﻿using Semaforos.ViewModels.Commands;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semaforos.ViewModels
+{
+    public class ViewModelBase
+    {
+        public SimpleComand SimpleComand { get; set; }
+
+        public ViewModelBase()
+        {
+            this.SimpleComand = new SimpleComand(this);
+        }
+
+        public void SimpreMethod()
+        {
+            Debug.WriteLine("Comando...");
+        }
+    }
+}
